e6f0dbe498d6d8cce6022984127686eca564d178
Leendert de Borst <ldeborst@xivisoft.com>
2024-04-29 15:10:04 +0200

    Update folder structure

---
+++ src/SpamOK.PasswordGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SpamOK.PasswordGenerator.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
